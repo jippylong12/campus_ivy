0e668ed291b9e9f84249004d4364e7689e5dd218
marcus.salinas <12.marcus.salinas@gmail.com>
2025-12-16 09:31:11 -0600

    Initial gem scaffold for campus_ivy

---
+++ lib/campus_ivy.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "campus_ivy/version"
+
+module CampusIvy
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "campus_ivy"
+
+require "minitest/autorun"
